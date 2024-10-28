e6f0fd345fc3f452b91264b23b238b35691ebf90
Seiltanzer01 <tizinallergia@gmail.com>
2024-10-28 23:16:09 +0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,5 @@
 // static/scripts.js
+
 $(document).ready(function() {
     // Пример анимации при наведении на строки таблицы
     $('table tr').hover(
@@ -29,4 +30,36 @@ $(document).ready(function() {
             $(this).css('display', 'none');
         }
     });
+
+    // Раскрывающиеся списки для категорий и подкатегорий
+    $('.collapse-button').click(function(){
+        $(this).next().toggle();
+    });
+
+    // Инициализация datepickers
+    $("#trade_open_time, #trade_close_time").datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+
+    // Telegram WebApp взаимодействие
+    if (window.Telegram.WebApp) {
+        // Инициализация WebApp
+        Telegram.WebApp.ready();
+
+        // Отправка данных пользователя на сервер
+        $.ajax({
+            url: '/telegram_auth',
+            method: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(Telegram.WebApp.initDataUnsafe.user),
+            success: function(response) {
+                console.log('Пользователь авторизован через Telegram WebApp.');
+                window.location.href = '/';
+            },
+            error: function(error) {
+                console.log('Ошибка авторизации через Telegram WebApp.');
+            }
+        });
+    }
 });
