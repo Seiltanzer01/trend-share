ae79c420845f851365fa75b4e6520c5b6bad5dbc
Seiltanzer01 <tizinallergia@gmail.com>
2024-11-08 01:36:28 +0300

    Merge multiple Alembic heads

---
+++ migrations/versions/5d8e410c0ad1_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: 5d8e410c0ad1
+Revises: 
+Create Date: 2024-11-08 01:34:22.633377
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5d8e410c0ad1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
