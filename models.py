1a49e91f12fd4b0ddad3cb234bd6baa2e7ce5bf6
Seiltanzer01 <tizinallergia@gmail.com>
2024-11-11 20:53:16 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,77 +4,81 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Ассоциативная таблица для связи сделок и критериев
 trade_criteria = db.Table('trade_criteria',
     db.Column('trade_id', db.Integer, db.ForeignKey('trade.id'), primary_key=True),
     db.Column('criterion_id', db.Integer, db.ForeignKey('criterion.id'), primary_key=True)
 )
 
+# Ассоциативная таблица для связи сетапов и критериев
 setup_criteria = db.Table('setup_criteria',
     db.Column('setup_id', db.Integer, db.ForeignKey('setup.id'), primary_key=True),
     db.Column('criterion_id', db.Integer, db.ForeignKey('criterion.id'), primary_key=True)
 )
 
 class User(db.Model):
-    __tablename__ = 'user'  # Явно указываем имя таблицы
     id = db.Column(db.Integer, primary_key=True)
-    telegram_id = db.Column(db.BigInteger, unique=True, nullable=False)  # Изменено на BigInteger
-    username = db.Column(db.String(80), unique=True, nullable=True)
+    telegram_id = db.Column(db.Integer, unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=True)
     first_name = db.Column(db.String(80), nullable=True)
     last_name = db.Column(db.String(80), nullable=True)
-    auth_token = db.Column(db.String(64), unique=True, nullable=True)
-    auth_token_creation_time = db.Column(db.DateTime, nullable=True)
-    registered_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())  # Добавлено поле регистрации
+    registered_at = db.Column(db.DateTime, nullable=False)
+
     trades = db.relationship('Trade', backref='user', lazy=True)
     setups = db.relationship('Setup', backref='user', lazy=True)
 
 class InstrumentCategory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(80), nullable=False)
+
     instruments = db.relationship('Instrument', backref='category', lazy=True)
 
 class Instrument(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(80), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('instrument_category.id'), nullable=False)
+
     trades = db.relationship('Trade', backref='instrument', lazy=True)
+
+class CriterionCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    subcategories = db.relationship('CriterionSubcategory', backref='category', lazy=True)
+
+class CriterionSubcategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('criterion_category.id'), nullable=False)
+
+    criteria = db.relationship('Criterion', backref='subcategory', lazy=True)
+
+class Criterion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    subcategory_id = db.Column(db.Integer, db.ForeignKey('criterion_subcategory.id'), nullable=False)
+
+    trades = db.relationship('Trade', secondary=trade_criteria, backref=db.backref('criteria', lazy='dynamic'))
+    setups = db.relationship('Setup', secondary=setup_criteria, backref=db.backref('criteria', lazy='dynamic'))
 
 class Trade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     instrument_id = db.Column(db.Integer, db.ForeignKey('instrument.id'), nullable=False)
-    direction = db.Column(db.String(10))
-    entry_price = db.Column(db.Float)
+    direction = db.Column(db.String(10), nullable=False)  # Buy или Sell
+    entry_price = db.Column(db.Float, nullable=False)
     exit_price = db.Column(db.Float, nullable=True)
-    trade_open_time = db.Column(db.Date)
+    trade_open_time = db.Column(db.Date, nullable=False)
     trade_close_time = db.Column(db.Date, nullable=True)
     comment = db.Column(db.Text, nullable=True)
     setup_id = db.Column(db.Integer, db.ForeignKey('setup.id'), nullable=True)
-    screenshot = db.Column(db.String(100), nullable=True)
+    screenshot = db.Column(db.String(120), nullable=True)
     profit_loss = db.Column(db.Float, nullable=True)
     profit_loss_percentage = db.Column(db.Float, nullable=True)
-    criteria = db.relationship('Criterion', secondary=trade_criteria, backref='trades')
 
 class Setup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    setup_name = db.Column(db.String(100))
+    setup_name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    screenshot = db.Column(db.String(100), nullable=True)
-    criteria = db.relationship('Criterion', secondary=setup_criteria, backref='setups')
-    trades = db.relationship('Trade', backref='setup', lazy=True)
-
-class CriterionCategory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    subcategories = db.relationship('CriterionSubcategory', backref='category', lazy=True)
-
-class CriterionSubcategory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    category_id = db.Column(db.Integer, db.ForeignKey('criterion_category.id'), nullable=False)
-    criteria = db.relationship('Criterion', backref='subcategory', lazy=True)
-
-class Criterion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    subcategory_id = db.Column(db.Integer, db.ForeignKey('criterion_subcategory.id'), nullable=False)
+    screenshot = db.Column(db.String(120), nullable=True)
