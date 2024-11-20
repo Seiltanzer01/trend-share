001612346bcf10d6d3b0001493feed02fdac3f33
Seiltanzer01 <tizinallergia@gmail.com>
2024-11-20 23:42:53 +0300

    Update scripts.js
    Обновлённый JavaScript-файл для обработки модальных окон и инициализации Datepicker.

--- static/scripts.js
+++ static/scripts.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
         }
     })();
 
-    // Обработчик для кнопки "Показать/Скрыть Фильтры"
+    // Обработчик для кнопки "Показать/Скрыть Фильтры" (если используется)
     $('#toggle-filters').click(function(){
         $('#filters').slideToggle();
         const buttonText = $(this).text();
