7189d5bb97b34acb7116a47f3579fc23bd2110f3
Seiltanzer01 <tizinallergia@gmail.com>
2024-12-28 00:32:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 # Импорт моделей и форм
 import models  # Убедитесь, что models.py импортирует db из extensions.py
 from poll_functions import start_new_poll, process_poll_results, update_real_prices_for_active_polls
+from staking_listener import scan_for_staking_transfers
 
 ADMIN_TELEGRAM_IDS = [427032240]
 
@@ -538,13 +539,12 @@ scheduler.add_job(
     next_run_time=datetime.utcnow() + timedelta(minutes=1)  # Запуск через 1 минуту после старта
 )
 
-from staking_listener import scan_for_staking_transfers
-
+# Планирование задачи сканирования входящих стейков каждые 1 минуту
 scheduler.add_job(
-    id='Scan for Staking Transfers',
-    func=lambda: scan_for_staking_transfers(app),
+    id='Scan For Staking Transfers',
+    func=scan_for_staking_transfers,
     trigger='interval',
-    minutes=1,  # Раз в минуту
+    minutes=1,
     next_run_time=datetime.utcnow() + timedelta(seconds=30)
 )
 # Запуск планировщика
